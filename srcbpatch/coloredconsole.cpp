2050bceef020a7f240d0c7ce79248305cb89ddbe
Alexey Zaytsev <Zaytsev.Lesha@gmail.com>
2024-02-02 00:02:55 +0100

    Colored output for ERROR and Warning - implemented

--- srcbpatch/coloredconsole.cpp
+++ srcbpatch/coloredconsole.cpp
@@ -10,50 +10,64 @@
 namespace coloredconsole
 {
 #ifndef __linux__
-    class WindowsConsoleColor
+    /// <summary>
+    ///   RAII for set console output color in windows
+    /// </summary>
+    class WindowsConsoleColor final
     {
+        WindowsConsoleColor(const WindowsConsoleColor&) = delete;
+        WindowsConsoleColor(WindowsConsoleColor&&) = delete;
+        WindowsConsoleColor& operator =(const WindowsConsoleColor&) = delete;
+        WindowsConsoleColor& operator =(WindowsConsoleColor&&) = delete;
     public:
-        WindowsConsoleColor(WORD color): hConsole(GetStdHandle(STD_OUTPUT_HANDLE))
+        /// <summary>
+        ///   set color
+        /// </summary>
+        /// <param name="color">color to set</param>
+        WindowsConsoleColor(WORD color): hConsole_(GetStdHandle(STD_OUTPUT_HANDLE))
         {
-            GetConsoleScreenBufferInfo(hConsole, &csbi);
-            originalColor = csbi.wAttributes;
-            SetConsoleTextAttribute(hConsole, color);
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(hConsole_, &csbi);
+            originalColor_ = csbi.wAttributes;
+            SetConsoleTextAttribute(hConsole_, color);
         }
 
+        /// <summary>
+        ///   set color back
+        /// </summary>
         ~WindowsConsoleColor()
         {
-            SetConsoleTextAttribute(hConsole, originalColor);
+            SetConsoleTextAttribute(hConsole_, originalColor_);
         }
 
-    private:
-        HANDLE hConsole;
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        WORD originalColor;
+    protected:
+        HANDLE hConsole_;
+        WORD originalColor_;
     };
 #endif
 
     void ColorizeWords(std::ostream& os, const std::string_view& sv)
     {
-        static const std::string_view error = "ERROR";
-        static const std::string_view warning = "Warning";
+        static const std::string_view errorSV = "ERROR";
+        static const std::string_view warningSV = "Warning";
 
         size_t pos = 0;
         while (pos < sv.size())
         {
-            size_t errorPos = sv.find(error, pos);
-            size_t warningPos = sv.find(warning, pos);
+            size_t errorPos = sv.find(errorSV, pos);
+            size_t warningPos = sv.find(warningSV, pos);
             if (errorPos != std::string_view::npos && (warningPos == std::string_view::npos || errorPos < warningPos))
             {
                 os << sv.substr(pos, errorPos - pos);
 
 #ifdef __linux__
-                os << "\033[1;31m" << error << "\033[0m";
+                os << "\033[1;31m" << errorSV << "\033[0m";
 #else
                 WindowsConsoleColor color(FOREGROUND_RED | FOREGROUND_INTENSITY);
-                os << error;
+                os << errorSV;
 #endif
 
-                pos = errorPos + error.size();
+                pos = errorPos + errorSV.size();
                 continue;
             }
 
@@ -62,19 +76,20 @@ namespace coloredconsole
                 os << sv.substr(pos, warningPos - pos);
 
 #ifdef __linux__
-                os << "\033[1;33m" << warning << "\033[0m";
+                os << "\033[1;33m" << warningSV << "\033[0m";
 #else
                 WindowsConsoleColor color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-                os << warning;
+                os << warningSV;
 #endif
 
-                pos = warningPos + warning.size();
+                pos = warningPos + warningSV.size();
                 continue;
             }
 
             os << sv.substr(pos, sv.size() - pos);
             pos = sv.size();
-        }
+        } // while (pos < sv.size())
+
     }// void ColorizeWords(std::ostream& os, const std::string_view& sv)
 
 } // namespace coloredconsole
